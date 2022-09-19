df38821b83e1470b3b0020aea8b4c78fb7a80596
1tang2bang92 <shpark0906@naver.com>
2022-09-19 16:56:15 +0900

    swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core'
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+} from '@nestjs/platform-fastify'
+import { AppModule } from './app.module'
+import setupSwagger from './swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true })
-  );
-  await app.listen(3000, '0.0.0.0');
+    new FastifyAdapter({ logger: false }),
+  )
+  setupSwagger(app)
+  await app.listen(3000, '0.0.0.0')
 }
-bootstrap();
+bootstrap()
